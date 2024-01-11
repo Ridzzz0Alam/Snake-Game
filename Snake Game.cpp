af41a8630246ddf20a033ab138f4ca0f2a53efff
Mohammad Reduwan <ridwan0gts@gmail.com>
2024-01-12 01:23:41 +0800

    Added changing fruit location

--- Snake Game.cpp
+++ Snake Game.cpp
@@ -103,6 +103,13 @@ void Logic()
 	if (x > width || x < 0 || y > height || y < 0)
 		gameOver = true;
 
+	if (x == fruitX && y == fruitY) 
+	{
+		score += 10;
+		fruitX = rand() % width;
+		fruitY = rand() % height;
+	}
+
 }
 
 int main() {
