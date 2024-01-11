40290dcde1c9cc702b698bd2deaca8e04d764274
Mohammad Reduwan <ridwan0gts@gmail.com>
2024-01-12 01:20:01 +0800

    Added moving snake

--- Snake Game.cpp
+++ Snake Game.cpp
@@ -28,7 +28,8 @@ void Draw() {
 	cout << endl;
 
 	for (int i = 0; i < height; i++) {
-		for (int j = 0; j < width; j++) {
+		for (int j = 0; j < width; j++) 
+		{
 			if (j == 0) {
 				cout << "#";
 			}
